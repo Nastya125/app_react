6d05347abe46f8f51085977a643e4d4513ae16c7
Nastya125 <nastyatrue@mail.ru>
2022-12-05 21:07:46 +0300

    commit React project

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere labore adipisci mollitia dolor qui natus, dignissimos at quaerat? Consequuntur aspernatur provident consectetur ullam voluptates. Accusantium earum explicabo ut ullam facere aliquid, consequatur quaerat magnam debitis. Corporis animi velit reprehenderit enim, odio cumque. Velit, ipsa soluta et neque perspiciatis maxime iusto?</div>    
+    )
+}
